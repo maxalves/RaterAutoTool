afdec7b036d081429aee7a36d394d190901ee5bf
Max <max.phazen@gmail.com>
2019-01-14 21:00:01 -0200

    buttons and slider values/getters

--- src/main/java/br/taskmanager/app/controller/ConfigController.java
+++ src/main/java/br/taskmanager/app/controller/ConfigController.java
@@ -3,22 +3,67 @@ package br.taskmanager.app.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXToggleButton;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 public class ConfigController implements Initializable {
 
-	/**
-	 * Initializes the controller class.
-	 */
+	@FXML
+	private JFXSlider refreshSlider, submitSlider;
+	@FXML
+	private JFXToggleButton refreshToggle, acquireToggle, openLinksToggle, submitToggle;
+	
+	private boolean ToggleBtn1, ToggleBtn2, ToggleBtn3, ToggleBtn4;
+	private double refreshRateSlider, submitPercentageSlider;
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
+		refreshSlider.setValue(10);
+		submitSlider.setValue(90);
 	}
 
 	@FXML
 	private void close(ActionEvent event) {
 	}
+	
+	@FXML
+	private void saveBtn(MouseEvent event) {
+		refreshRateSlider = refreshSlider.getValue();
+		submitPercentageSlider = submitSlider.getValue();
+		ToggleBtn1 = refreshToggle.isSelected();
+		ToggleBtn2 = acquireToggle.isSelected();
+		ToggleBtn3 = openLinksToggle.isSelected();
+		ToggleBtn4 = submitToggle.isSelected();
+	}
+
+	public boolean isToggleBtn1() {
+		return ToggleBtn1;
+	}
+
+	public boolean isToggleBtn2() {
+		return ToggleBtn2;
+	}
+
+	public boolean isToggleBtn3() {
+		return ToggleBtn3;
+	}
+
+	public boolean isToggleBtn4() {
+		return ToggleBtn4;
+	}
+
+	public double getRefreshRateSlider() {
+		return refreshRateSlider;
+	}
+
+	public double getSubmitPercentageSlider() {
+		return submitPercentageSlider;
+	}
+
 
 }
