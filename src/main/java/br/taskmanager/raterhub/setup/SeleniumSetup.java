bc1d521b2fcb18b1bd8f9d2623848ff99cfa2cde
Max <max.phazen@gmail.com>
2019-01-15 14:27:48 -0200

    Selenium class responsible for UI data and selenium setup

---
+++ src/main/java/br/taskmanager/raterhub/setup/SeleniumSetup.java
@@ -0,0 +1,43 @@
+package br.taskmanager.raterhub.setup;
+
+import org.openqa.selenium.WebDriver;
+
+import br.taskmanager.raterhub.pages.MainPage;
+
+public class SeleniumSetup {
+	private MainPage raterWebpage;
+	private WebDriver driver;
+	private boolean refresh, acquire, openTaskLinks, submit;
+	private Double refreshRate, submitPercentage;
+	private String chromeDriverLocation = "C:\\chromedriver.exe";
+	private String chromeExeLocation = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\\"";
+	private String userDataDirLocation = "C:\\selenium\\AutomationProfile";
+	private String port = "9222";
+
+	public void seleniumWebDriverConfig(boolean refresh, boolean acquire, boolean openTaskLinks, boolean submit,
+			Double refreshRate, Double submitPercentage) {
+		this.refresh = refresh;
+		this.acquire = acquire;
+		this.openTaskLinks = openTaskLinks;
+		this.submit = submit;
+		this.refreshRate = refreshRate;
+		this.submitPercentage = submitPercentage;
+		
+		SingletonBrowserSetup instance = SingletonBrowserSetup.getInstance(chromeDriverLocation, chromeExeLocation,
+				userDataDirLocation, port);
+		driver = instance.getDriver();
+		
+		startAutomation();
+	}
+
+	public void closeSelenium() {
+		driver.close();
+		driver.quit();
+	}
+
+	private void startAutomation() {
+		raterWebpage = new MainPage(driver);
+		raterWebpage.autoRefreshUntilTask(refresh, refreshRate);
+		raterWebpage.autoAcquireTask(acquire);
+	}
+}
