9de9929b0086e1f6b20f88b31a67371661ad9ba9
Max <max.phazen@gmail.com>
2019-01-15 14:24:14 -0200

    thread for selenium chrome webdriver

--- src/main/java/br/taskmanager/app/controller/ConfigController.java
+++ src/main/java/br/taskmanager/app/controller/ConfigController.java
@@ -6,7 +6,9 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXToggleButton;
 
-import javafx.event.ActionEvent;
+import br.taskmanager.raterhub.setup.SeleniumSetup;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,7 +21,9 @@ public class ConfigController implements Initializable {
 	private JFXSlider refreshSlider, submitSlider;
 	@FXML
 	private JFXToggleButton refreshToggle, acquireToggle, openLinksToggle, submitToggle;
-	
+
+	private SeleniumSetup seleniumSetup = new SeleniumSetup();
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		refreshSlider.setValue(10);
@@ -28,10 +32,28 @@ public class ConfigController implements Initializable {
 
 	@FXML
 	private void closeBtn(MouseEvent event) {
-		((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+		((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+		seleniumSetup.closeSelenium();
 	}
-	
+
 	@FXML
 	private void saveBtn(MouseEvent event) {
+		sendConfigData.restart();
 	}
+
+	// thread responsible to send configuration data to SeleniumSetup.class
+	Service<Void> sendConfigData = new Service<Void>() {
+		@Override
+		protected Task<Void> createTask() {
+			return new Task<Void>() {
+				@Override
+				protected Void call() throws Exception {
+					seleniumSetup.seleniumWebDriverConfig(refreshToggle.isSelected(), acquireToggle.isSelected(),
+							openLinksToggle.isSelected(), submitToggle.isSelected(), refreshSlider.getValue(),
+							submitSlider.getValue());
+					return null;
+				}
+			};
+		}
+	};
 }
