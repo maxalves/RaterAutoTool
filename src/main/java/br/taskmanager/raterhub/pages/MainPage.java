68b861b23596d00d1381df2d85203e482d1407ed
Max <max.phazen@gmail.com>
2019-01-16 15:22:53 -0200

    Loop automation and timeoutException handler

--- src/main/java/br/taskmanager/raterhub/pages/MainPage.java
+++ src/main/java/br/taskmanager/raterhub/pages/MainPage.java
@@ -5,41 +5,60 @@ import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MainPage {
 
 	private WebDriver driver;
+	public String mainURL = "https://raterhub.com/evaluation/rater";
 
 	public MainPage(WebDriver driver) {
 		this.driver = driver;
 	}
-	
-	//If mainURL != current URL then it doesn't refresh
-	public void autoRefreshUntilTask(boolean refresh, Double refreshRate, String mainURL) {
-		
-		//Trying to figure how to test if i am in a specific page
-		String teste = driver.getCurrentUrl();
-		
+
+	public void autoRefreshUntilTask(boolean refresh, Double refreshRate) throws TimeoutException {
 		if (refresh) {
 			Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofHours(24))
 					.pollingEvery(Duration.ofSeconds(refreshRate.intValue())).ignoring(NoSuchElementException.class);
 
-			WebElement taskAvailable = wait.until(new Function<WebDriver, WebElement>() {
+			wait.until(new Function<WebDriver, WebElement>() {
 				public WebElement apply(WebDriver driver) {
-					driver.navigate().refresh();
-					return driver.findElement(By.className("ewok-rater-task-option"));
+					if (isMainPage()) {
+						driver.navigate().refresh();
+						return driver.findElement(By.className("ewok-rater-task-option"));
+					} else {
+						return null;
+					}
 				}
 			});
 		}
 	}
 
-	public void autoAcquireTask(boolean acquire, String mainURL) {
-		if (acquire) {
+	// Waits until task button shows even if autorefresh is disabled
+	public void autoAcquireTask(boolean acquire) throws TimeoutException{
+		if (acquire && isMainPage()) { 
+			WebDriverWait wait = new WebDriverWait(driver,  10);
+			System.out.println("esperando aparecer");
+			wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.ewok-rater-task-option>li>a")));
 			driver.findElement(By.cssSelector("ul.ewok-rater-task-option>li>a")).click();
+		}
+	}
+
+	// Check if it's a mainpage and returns false if it isn't
+	private boolean isMainPage() {
+		boolean taskURL = driver.getCurrentUrl().contains("/task/show/");
+		boolean mainpageURL = driver.getCurrentUrl().contains("C:/Users/max_a/Desktop/HTMLPage");
+
+		if (!taskURL && mainpageURL) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
