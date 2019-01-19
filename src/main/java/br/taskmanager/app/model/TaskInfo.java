06c09700c8c0ee49d84afe8a10e12e195d9bca22
Max <max.phazen@gmail.com>
2019-01-18 23:18:31 -0200

    Task overview

---
+++ src/main/java/br/taskmanager/app/model/TaskInfo.java
@@ -0,0 +1,45 @@
+package br.taskmanager.app.model;
+
+import java.time.LocalDate;
+
+public class TaskInfo {
+
+	private LocalDate date;
+	private String type;
+	private int numberCompleted;
+
+	public int getNumberCompleted() {
+		return numberCompleted;
+	}
+
+	public void setNumberCompleted(int numberCompleted) {
+		this.numberCompleted = numberCompleted;
+	}
+
+	private Double totalTime;
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(Double totalTime) {
+		this.totalTime = totalTime;
+	}
+
+}
