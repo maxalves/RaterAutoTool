d6236dbe47cef1f5133bc731af0ff053c6522290
Max <max.phazen@gmail.com>
2019-01-22 15:31:03 -0200

    Main url check not needed

--- src/main/java/br/taskmanager/selenium/pages/MainPage.java
+++ src/main/java/br/taskmanager/selenium/pages/MainPage.java
@@ -1,4 +1,4 @@
-package br.taskmanager.raterhub.pages;
+package br.taskmanager.selenium.pages;
 
 import java.awt.Toolkit;
 import java.time.Duration;
@@ -20,27 +20,20 @@ public class MainPage {
 	private WebDriver driver;
 	private String selectorTaskAvailable = "div[class='container'] ul";
 	private String selectorAcquire = "div[class='container'] ul.ewok-rater-task-option>li>a";
-	public String mainURL = "https://www.raterhub.com/evaluation/rater";
 
-	public MainPage(WebDriver driver) {
+	public MainPage(WebDriver driver, String mainURL) {
 		this.driver = driver;
 	}
 
 	public void autoRefreshUntilTask(boolean refresh, Double refreshRate) throws TimeoutException {
 		if (refresh) {
-			Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofHours(24))
+			Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds((long) (refreshRate*2)))
 					.pollingEvery(Duration.ofSeconds(refreshRate.intValue())).ignoring(NoSuchElementException.class);
 
 			wait.until(new Function<WebDriver, WebElement>() {
 				public WebElement apply(WebDriver driver) {
-					if (isMainPage()) {
-						if (driver.findElements(By.cssSelector(selectorTaskAvailable)).size() == 0) {
-							driver.navigate().refresh();
-						}
+						driver.navigate().refresh();
 						return driver.findElement(By.cssSelector(selectorTaskAvailable));
-					} else {
-						return null;
-					}
 				}
 			});
 		}
@@ -48,24 +41,11 @@ public class MainPage {
 
 	// Waits until task button shows even if autorefresh is disabled
 	public void autoAcquireTask(boolean acquire) throws TimeoutException {
-		if (acquire && isMainPage()) {
+		if (acquire) {
 			WebDriverWait wait = new WebDriverWait(driver, 2);
 			Toolkit.getDefaultToolkit().beep();
 			wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(selectorAcquire)));
 			driver.findElement(By.cssSelector(selectorAcquire)).sendKeys(Keys.ENTER);
-		}
-	}
-
-	// Check if it's a mainpage and returns false if it isn't
-	private boolean isMainPage() {
-		boolean taskURL = driver.getCurrentUrl().contains("/task/show");
-		boolean taskURL2 = driver.getCurrentUrl().contains("/task/new");
-		boolean mainpageURL = driver.getCurrentUrl().contains(mainURL);
-
-		if (!taskURL && mainpageURL && !taskURL2) {
-			return true;
-		} else {
-			return false;
 		}
 	}
 }
