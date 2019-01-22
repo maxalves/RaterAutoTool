05011165affa36703551cc7b792f0134bb743516
Max <max.phazen@gmail.com>
2019-01-22 15:36:46 -0200

    check page before each loop

--- src/main/java/br/taskmanager/selenium/setup/SeleniumStartup.java
+++ src/main/java/br/taskmanager/selenium/setup/SeleniumStartup.java
@@ -1,13 +1,16 @@
-package br.taskmanager.raterhub.setup;
+package br.taskmanager.selenium.setup;
 
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 
-import br.taskmanager.raterhub.pages.MainPage;
+import br.taskmanager.app.model.Task;
+import br.taskmanager.selenium.pages.MainPage;
+import br.taskmanager.selenium.pages.TaskPage;
 
-public class SeleniumSetup {
-	private MainPage raterWebpage;
+public class SeleniumStartup {
+	private MainPage mainPage;
+	private TaskPage taskPage;
 	private WebDriver driver = null;
 	private boolean refresh, acquire, openTaskLinks, submit;
 	private Double refreshRate, submitPercentage;
@@ -15,6 +18,7 @@ public class SeleniumSetup {
 	private String chromeExeLocation = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\\"";
 	private String userDataDirLocation = "C:\\selenium\\AutomationProfile";
 	private String port = "9222";
+	private String mainURL = "https://www.raterhub.com/evaluation/rater";
 
 	public void seleniumWebDriverConfig(boolean refresh, boolean acquire, boolean openTaskLinks, boolean submit,
 			Double refreshRate, Double submitPercentage) {
@@ -28,15 +32,21 @@ public class SeleniumSetup {
 		SingletonBrowserSetup instance = SingletonBrowserSetup.getInstance(chromeDriverLocation, chromeExeLocation,
 				userDataDirLocation, port);
 		driver = instance.getDriver();
-
+		mainPage = new MainPage(driver, mainURL);
+		taskPage = new TaskPage(driver, mainURL);
+		driver.get(mainURL);
+		
 		startAutomation();
 	}
 
 	public void closeSelenium() {
 		try {
-			driver.close();
+			for (String handle : driver.getWindowHandles()) {
+				driver.switchTo().window(handle);
+				driver.close();
+			}
 		} catch (RuntimeException e) {
-			System.out.println("Driver is already closed!");
+			System.out.println("Driver is closed!");
 			Thread.currentThread().interrupt();
 		}
 
@@ -47,21 +57,40 @@ public class SeleniumSetup {
 	}
 
 	private void startAutomation() {
-		raterWebpage = new MainPage(driver);
-		driver.get(raterWebpage.mainURL);
-
 		for (;;) {
 			try {
-				raterWebpage.autoRefreshUntilTask(refresh, refreshRate);
-				raterWebpage.autoAcquireTask(acquire);
-				waitAndSync();
+				if (whatPageIamAt() == "mainpage") {
+					System.out.println("teste");
+					mainPage.autoRefreshUntilTask(refresh, refreshRate);
+					System.out.println("teste2");
+					mainPage.autoAcquireTask(acquire);
+					waitAndSync();
+				} else if (whatPageIamAt() == "taskpage") {
+					
+				} else {
+					waitAndSync();
+				}
 			} catch (TimeoutException e) {
-				System.out.println("Automation Timeout");
+				System.out.println("Timeout -> Retry");
 				waitAndSync();
 			} catch (WebDriverException e) {
-				System.out.println("Selenium Thread Stopped");
+				System.out.println("Selenium Stopped -> Retry");
 				waitAndSync();
 			}
+		}
+	}
+	
+	private String whatPageIamAt() {
+		boolean taskURL = driver.getCurrentUrl().contains("/task/show");
+		boolean taskURL2 = driver.getCurrentUrl().contains("/task/new");
+		boolean mainpageURL = driver.getCurrentUrl().contains(mainURL);
+
+		if (!taskURL && mainpageURL && !taskURL2) {
+			return "mainpage";
+		} else if (taskURL && mainpageURL){
+			return "taskpage";
+		} else {
+			return null;
 		}
 	}
 
