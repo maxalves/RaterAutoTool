8ef6c524bf07e77f6fb99d210db0995cb4a649b7
Max <max.phazen@gmail.com>
2019-01-24 16:06:23 -0200

    Open Links and submit method

--- src/main/java/br/taskmanager/selenium/setup/SeleniumStartup.java
+++ src/main/java/br/taskmanager/selenium/setup/SeleniumStartup.java
@@ -1,9 +1,19 @@
 package br.taskmanager.selenium.setup;
 
+import java.awt.Toolkit;
+import java.sql.Connection;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
-
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import br.taskmanager.app.connectionfactory.ConnectionFactory;
+import br.taskmanager.app.dao.TaskDao;
 import br.taskmanager.app.model.Task;
 import br.taskmanager.selenium.pages.MainPage;
 import br.taskmanager.selenium.pages.TaskPage;
@@ -11,6 +21,8 @@ import br.taskmanager.selenium.pages.TaskPage;
 public class SeleniumStartup {
 	private MainPage mainPage;
 	private TaskPage taskPage;
+	Connection connection = ConnectionFactory.connector();
+	private TaskDao taskDao = new TaskDao(connection);
 	private WebDriver driver = null;
 	private boolean refresh, acquire, openTaskLinks, submit;
 	private Double refreshRate, submitPercentage;
@@ -32,10 +44,10 @@ public class SeleniumStartup {
 		SingletonBrowserSetup instance = SingletonBrowserSetup.getInstance(chromeDriverLocation, chromeExeLocation,
 				userDataDirLocation, port);
 		driver = instance.getDriver();
-		mainPage = new MainPage(driver, mainURL);
-		taskPage = new TaskPage(driver, mainURL);
+		mainPage = new MainPage(driver);
+		taskPage = new TaskPage(driver);
 		driver.get(mainURL);
-		
+
 		startAutomation();
 	}
 
@@ -45,6 +57,7 @@ public class SeleniumStartup {
 				driver.switchTo().window(handle);
 				driver.close();
 			}
+			driver.quit();
 		} catch (RuntimeException e) {
 			System.out.println("Driver is closed!");
 			Thread.currentThread().interrupt();
@@ -60,16 +73,25 @@ public class SeleniumStartup {
 		for (;;) {
 			try {
 				if (whatPageIamAt() == "mainpage") {
-					System.out.println("teste");
 					mainPage.autoRefreshUntilTask(refresh, refreshRate);
-					System.out.println("teste2");
 					mainPage.autoAcquireTask(acquire);
-					waitAndSync();
-				} else if (whatPageIamAt() == "taskpage") {
-					
-				} else {
-					waitAndSync();
+				} else if (true) {
+					Task task = new Task();
+					task = taskPage.getTaskData();
+					if (!isTaskAlreadyCompleted(task)) {
+						taskDao.add(task);
+						taskPage.openTaskLinks(openTaskLinks);
+						if (submit) {
+							System.out.println("wait submit");
+							waitUntilSubmitTime(task);
+						}
+					}
 				}
+				System.out.println("Sync -> Restart");
+				waitAndSync();
+			} catch (NoSuchElementException e) {
+				System.out.println("Element not Found");
+				waitAndSync();
 			} catch (TimeoutException e) {
 				System.out.println("Timeout -> Retry");
 				waitAndSync();
@@ -79,6 +101,40 @@ public class SeleniumStartup {
 			}
 		}
 	}
+
+	private void waitUntilSubmitTime(Task task) {
+		ExpectedCondition<Boolean> timeCondition = new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				if (formatTimeAsDouble(taskPage.getCurrentTime()) >= considerSubmitPercentage(task.getTime())) {
+					System.out.println("Autosubmit Task!");
+					Toolkit.getDefaultToolkit().beep();
+					taskPage.submit();
+					return true;
+				} else if (driver.getCurrentUrl().contains(String.valueOf(task.getId()))) {
+					//if i am still in the same page then user didn't submit yet
+					return false;
+				} else {
+					System.out.println("User submitted task!");
+					return true;
+				}
+			}
+		};
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofMinutes(task.getTime().intValue()+1))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+
+		wait.until(timeCondition);
+	}
+
+	private boolean isTaskAlreadyCompleted(Task task) {
+		List<Task> listThreeTasks = taskDao.getlastThreeTasksAdded();
+		for (Task tasks : listThreeTasks) {
+			if (tasks.getId() == task.getId()) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	private String whatPageIamAt() {
 		boolean taskURL = driver.getCurrentUrl().contains("/task/show");
@@ -87,16 +143,27 @@ public class SeleniumStartup {
 
 		if (!taskURL && mainpageURL && !taskURL2) {
 			return "mainpage";
-		} else if (taskURL && mainpageURL){
+		} else if (taskURL && mainpageURL) {
 			return "taskpage";
 		} else {
 			return null;
 		}
 	}
 
+	private Double formatTimeAsDouble(String currentTime) {
+		currentTime = currentTime.replaceAll(":", ".");
+		Double timeAsDouble = Double.parseDouble(currentTime);
+		return timeAsDouble;
+	}
+
+	private Double considerSubmitPercentage(Double timeToSubmit) {
+		timeToSubmit = (submitPercentage * timeToSubmit) / 100;
+		return timeToSubmit;
+	}
+
 	private void waitAndSync() {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 			throw new RuntimeException(e);
