dccda58f123db8edebc1df81344e152ee3f9666a
Max <max.phazen@gmail.com>
2019-01-25 16:34:14 -0200

    Close Request actions

--- src/main/java/br/taskmanager/app/main/LaunchApp.java
+++ src/main/java/br/taskmanager/app/main/LaunchApp.java
@@ -1,12 +1,16 @@
 package br.taskmanager.app.main;
 
+import br.taskmanager.selenium.setup.SingletonBrowserSetup;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 public class LaunchApp extends Application {
 
@@ -21,6 +25,15 @@ public class LaunchApp extends Application {
 		stage.initStyle(StageStyle.TRANSPARENT);
 		LaunchApp.stage = stage;
 		stage.show();
+		
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+            	SingletonBrowserSetup.getDriver().quit();
+                Platform.exit();
+            }
+
+        });
 	}
 
 	public static void main(String[] args) {
